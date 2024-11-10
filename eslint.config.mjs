9ac16e6382993edd0c43bba7bfcc66a9023580c0
Vordgi <sasha2822222@gmail.com>
2024-11-10 18:30:23 +0400

    npp-1 update eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,8 @@
 import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 import tseslint from "typescript-eslint";
 
-const ignores = ["**/node_modules/**", "**/dist/**", "**/*.js", "**/*.d.ts"];
-
 export default [
+    { ignores: ["**/node_modules/**", "**/dist/**", "**/*.js", "**/*.d.ts"] },
     {
         rules: {
             "prettier/prettier": [
@@ -19,4 +18,4 @@ export default [
     },
     ...tseslint.configs.recommended,
     eslintPluginPrettierRecommended,
-].map((r) => Object.assign(r, { ignores }));
+];
